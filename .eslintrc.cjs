41b58015d096443c25dde6fed50d1226182955f2
Dhruvi Sherathiya <dhruvi@auberginesolutions.com>
2024-02-21 17:42:20 +0530

    eslint and prettier setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'], // prettier
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    // allow to import devDependencies
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+};

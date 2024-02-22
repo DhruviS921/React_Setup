204eab7efd17b01b1e5a72adf817c76d8ca3f624
Dhruvi Sherathiya <dhruvi@auberginesolutions.com>
2024-02-22 15:49:38 +0530

    feature: added

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  rules: {
+    'type-enum': [2, 'always', ['feature', 'CER']],
+    'type-case': [2, 'always', ['upper-case', 'lower-case', 'sentence-case']],
+    'subject-case': [2, 'always', ['upper-case', 'lower-case', 'sentence-case']],
+  },
+};

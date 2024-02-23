9bef92b35750fc52fa2cd41d3cc7db1df29b24f8
Dhruvi Sherathiya <dhruvi@auberginesolutions.com>
2024-02-23 11:23:50 +0530

    CR-4 fix: Added rules for jira

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+-\d+)\s+(\w+):\s+(.*)$/,
+      headerCorrespondence: ['ticket', 'type', 'subject'],
+    },
+  },
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'refactor']],
     'type-case': [2, 'always', ['lower-case']],
     'subject-case': [2, 'always', ['upper-case', 'lower-case', 'sentence-case']],
-    // 'jira-ticket-id': [2, 'always', [/^CR-[0-9]+$/]],
   },
 };

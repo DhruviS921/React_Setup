9dcf296e60409d853419ff32b2f5cc233bea573f
Dhruvi Sherathiya <dhruvi@auberginesolutions.com>
2024-02-23 10:36:44 +0530

    feat: without jira id

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feature', 'CER']],
-    'type-case': [2, 'always', ['upper-case', 'lower-case', 'sentence-case']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor']],
+    'type-case': [2, 'always', ['lower-case']],
     'subject-case': [2, 'always', ['upper-case', 'lower-case', 'sentence-case']],
+    // 'jira-ticket-id': [2, 'always', [/^CR-[0-9]+$/]],
   },
 };

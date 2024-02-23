e74b659c9d528001450b8aee33ee369953031774
Dhruvi Sherathiya <dhruvi@auberginesolutions.com>
2024-02-23 12:25:19 +0530

    CR-4 feat: Branch name validations added

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,4 @@
+// valid comit msg format :- CR-4 fix: Added rules for jira
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   parserPreset: {
@@ -7,8 +8,9 @@ module.exports = {
     },
   },
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'refactor']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'style']],
     'type-case': [2, 'always', ['lower-case']],
     'subject-case': [2, 'always', ['upper-case', 'lower-case', 'sentence-case']],
+    'subject-max-length': [2, 'always', 50],
   },
 };
